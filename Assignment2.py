e8117e5f9144b5b76d03c092d443b8d965444e6b
Zainab Hammoud <106276048+Zainabhammoud@users.noreply.github.com>
2023-08-11 01:53:46 +0300

    Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -11,3 +11,19 @@ def reverse(s,i):
     new = reverse_s * i
   return new
 print("Reversed list is :" + reverse(" ",0))
+
+#2
+def sort(s):
+  s = input("Add the word: ")
+  lower = []
+  upper = []
+  for char in s:
+    if char.islower():
+        lower.append(char)
+    else:
+        upper.append(char)
+      
+  sorted_str = ''.join(upper + lower)
+  print('Result:', sorted_str)
+
+
